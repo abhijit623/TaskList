5c6b3151d66af0c455ce6853ef0dcfee0aae97e0
abhijit623 <40374399+abhijit623@users.noreply.github.com>
2018-06-19 00:02:10 -0600

    Added component

--- src/main/java/com/hometest/walmart/taskList/model/Sequence.java
+++ src/main/java/com/hometest/walmart/taskList/model/Sequence.java
@@ -1,8 +1,9 @@
 package com.hometest.walmart.taskList.model;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class Sequence {
 
     private static final AtomicInteger counter = new AtomicInteger();
